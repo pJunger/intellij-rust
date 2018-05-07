633f7e4a8f3fdc2a5f06a41ec2406ca8089773fa
Arseniy Pendryak <a.pendryak@yandex.ru>
2018-05-08 02:21:21 +0300

    T: check test run configurations

--- src/test/kotlin/org/rust/cargo/runconfig/RunConfigurationTestCase.kt
+++ src/test/kotlin/org/rust/cargo/runconfig/RunConfigurationTestCase.kt
@@ -5,16 +5,20 @@
 
 package org.rust.cargo.runconfig
 
+import com.intellij.execution.actions.ConfigurationContext
+import com.intellij.execution.actions.RunConfigurationProducer
 import com.intellij.execution.configurations.ConfigurationTypeUtil
 import com.intellij.execution.configurations.RunConfiguration
 import com.intellij.execution.executors.DefaultRunExecutor
 import com.intellij.execution.process.*
 import com.intellij.execution.runners.ExecutionEnvironmentBuilder
+import com.intellij.ide.DataManager
 import com.intellij.openapi.util.Disposer
 import com.intellij.openapi.util.Key
 import org.rust.cargo.RustWithToolchainTestBase
 import org.rust.cargo.runconfig.command.CargoCommandConfiguration
 import org.rust.cargo.runconfig.command.CargoCommandConfigurationType
+import org.rust.cargo.runconfig.test.CargoTestRunConfigurationProducer
 import org.rust.fileTree
 
 class RunConfigurationTestCase : RustWithToolchainTestBase() {
@@ -42,10 +46,130 @@ class RunConfigurationTestCase : RustWithToolchainTestBase() {
         check("Hello, world!" in result.stdout)
     }
 
+
+    fun `test single test configuration 1`() {
+        val testProject = fileTree {
+            toml("Cargo.toml", """
+                [package]
+                name = "hello"
+                version = "0.1.0"
+                authors = []
+            """)
+
+            dir("src") {
+                rust("main.rs", """
+                    fn main() {
+                        println!("Hello, world!");
+                    }
+
+                    #[test]
+                    fn foo() {
+                        /*caret*/
+                        println!("Foo");
+                    }
+                """)
+            }
+        }.create()
+        myFixture.configureFromTempProjectFile(testProject.fileWithCaret)
+
+        val configuration = createTestRunConfigurationFromContext()
+        val result = execute(configuration)
+        check("Foo" in result.stdout)
+    }
+
+    fun `test single test configuration 2`() {
+        val testProject = fileTree {
+            toml("Cargo.toml", """
+                [package]
+                name = "hello"
+                version = "0.1.0"
+                authors = []
+            """)
+
+            dir("src") {
+                rust("main.rs", """
+                    fn main() {
+                        println!("Hello, world!");
+                    }
+
+                    #[cfg(test)]
+                    mod tests {
+                        #[test]
+                        fn foo() {
+                            /*caret*/
+                            println!("Foo");
+                        }
+
+                        #[test]
+                        fn bar() {
+                            println!("Bar");
+                        }
+                    }
+                """)
+            }
+        }.create()
+        myFixture.configureFromTempProjectFile(testProject.fileWithCaret)
+
+        val configuration = createTestRunConfigurationFromContext()
+        val result = execute(configuration)
+        check("Foo" in result.stdout)
+        check("Bar" !in result.stdout)
+    }
+
+    fun `test mod test configuration`() {
+        val testProject = fileTree {
+            toml("Cargo.toml", """
+                [package]
+                name = "hello"
+                version = "0.1.0"
+                authors = []
+            """)
+
+            dir("src") {
+                rust("main.rs", """
+                    fn main() {
+                        println!("Hello, world!");
+                    }
+
+                    #[cfg(test)]
+                    mod tests {
+                        /*caret*/
+                        #[test]
+                        fn foo() {
+                            println!("Foo");
+                        }
+
+                        #[test]
+                        fn bar() {
+                            println!("Bar");
+                        }
+                    }
+                """)
+            }
+        }.create()
+        myFixture.configureFromTempProjectFile(testProject.fileWithCaret)
+
+        val configuration = createTestRunConfigurationFromContext()
+        val result = execute(configuration)
+        check("Foo" in result.stdout)
+        check("Bar" in result.stdout)
+    }
+
     private fun createConfiguration(): CargoCommandConfiguration {
         val configurationType = ConfigurationTypeUtil.findConfigurationType(CargoCommandConfigurationType::class.java)
         val factory = configurationType.factory
         return factory.createTemplateConfiguration(myModule.project) as CargoCommandConfiguration
+    }
+
+    private fun createTestRunConfigurationFromContext(): CargoCommandConfiguration =
+        createRunConfigurationFromContext(CargoTestRunConfigurationProducer())
+
+    private fun createRunConfigurationFromContext(producer: RunConfigurationProducer<CargoCommandConfiguration>): CargoCommandConfiguration {
+        val context = DataManager.getInstance().getDataContext(myFixture.editor.component)
+        return producer.createConfigurationFromContext(ConfigurationContext.getFromContext(context))
+                ?.configuration as? CargoCommandConfiguration
+                ?: error("Can't create run configuration")
+
     }
 
     private fun execute(configuration: RunConfiguration): ProcessOutput {
